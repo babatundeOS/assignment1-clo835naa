baf4bd346e990ecb2d69421e7270f4ef5c326977
Babatunde Seyi Oyeyemi <babatundeoyeyemi@gmail.com>
2024-02-04 10:48:03 +0000

    final cleanup and updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,6 @@ data "aws_ami" "latest_amazon_linux" {
 resource "aws_instance" "my_ec2_instance" {
   ami           = data.aws_ami.latest_amazon_linux.id
   instance_type = "t2.micro"
-  #security_groups = [aws_security_group.my_security_group.id]
   key_name      = aws_key_pair.my_key.key_name
   iam_instance_profile        = "LabInstanceProfile"
   tags = {
@@ -28,32 +27,6 @@ resource "aws_instance" "my_ec2_instance" {
 resource "aws_ecr_repository" "ecr_assignment1_repo" {
   name = "ecr_assignment1_repo"
   image_tag_mutability = "MUTABLE" 
-}
-
-resource "aws_security_group" "my_security_group" {
-  name        = "my-security-group"
-  description = "Allow inbound traffic on specified ports"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8083
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_key_pair" "my_key" {
